7b5ae5c38246a89fdf62abadbfa983279e4a5018
willian-rodrigues <willian.rsilva1994@gmail.com>
2022-06-21 13:43:18 -0300

    mobile drawer button styled

--- src/components/display/nav-bar/navigation-drawer.tsx
+++ src/components/display/nav-bar/navigation-drawer.tsx
@@ -5,7 +5,8 @@ import {
   ListItemIcon,
   ListItemText,
   Drawer,
-  Divider
+  Divider,
+  Typography
 } from '@mui/material';
 import { Link } from '../../ui/link/link.component';
 
@@ -32,22 +33,29 @@ export default function NavigationDrawer(props: NavigationDrawerProps) {
           justifyContent: 'flex-end',
           alignItems: 'center',
           px: 1,
-          // TODO: Remove hardcoded values when theme.mixins.toolbar gets adapted to MUI v5
           minHeight: { xs: 48, sm: 64 }
         }}
       ></Box>
       <Divider />
       <List>
         {menuItems.map(item => (
-          <ListItem
-            key={item.name}
-            button
-            onClick={onClose}
-            {...{ component: Link, href: item.link, noLinkStyle: true }}
-          >
-            <ListItemIcon>{item.icon}</ListItemIcon>
-            <ListItemText primary={item.name} />
-          </ListItem>
+          <Link key={item.name} href={item.link} onClick={onClose}>
+            <Box
+              display="flex"
+              alignItems="center"
+              gap={1}
+              px={2}
+              py={1}
+              width={160}
+            >
+              <Box width={'20%'}>
+                <Typography color="primary">{item.icon}</Typography>
+              </Box>
+              <Typography color="secondary" variant="body1" mr={'auto'}>
+                {item.name}
+              </Typography>
+            </Box>
+          </Link>
         ))}
       </List>
     </Drawer>
