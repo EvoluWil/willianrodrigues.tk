9eadca32828370dc54fdfc87b2bfddc7e624c265
willian-rodrigues <willian.rsilva1994@gmail.com>
2022-06-23 10:43:08 -0300

    remove follow cursor

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
 import { Box, ThemeProvider } from '@mui/material';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Footer } from '../components/display/footer/footer.component';
 import { NavBar } from '../components/display/nav-bar/nav-bar.component';
-import { CursorFollow } from '../components/ui/cursor-follow/cursor-follow.component';
 import { WhatsappFloatButton } from '../components/ui/whatsapp-float/whatsapp-float.dynamic';
 import { Global } from '../global/styles/globals';
 import theme from '../global/styles/theme';
@@ -23,7 +23,6 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         <Component {...pageProps} />
         <Footer />
         <WhatsappFloatButton />
-        <CursorFollow />
       </AppContainer>
     </ThemeProvider>
   );
